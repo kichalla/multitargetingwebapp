dda364eb07b437610eaee4bcd072eb3e1101e8fe
Kiran <kichalla@microsoft.com>
2021-08-03 10:00:29 -0700

    Moved action results to a separate file

---
+++ WebAPI.Service1/ActionResults.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+#if NETFRAMEWORK
+using System.Web.Http;
+#else
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+#endif
+
+namespace WebAPI.Service1
+{
+    public interface IHttpStatusCodeActionResult : IHttpActionResult
+    {
+        HttpStatusCode StatusCode { get; }
+    }
+
+#if !NETFRAMEWORK
+    public abstract class ApiController : ControllerBase { }
+
+    public interface IHttpActionResult { }
+
+    public class AcrObjectResult<TValue> : IHttpStatusCodeActionResult, IActionResult
+    {
+        public AcrObjectResult(HttpStatusCode statusCode, TValue value, HttpRequest request)
+        {
+            StatusCode = statusCode;
+            Value = value;
+            RequestMessage = request;
+        }
+
+        public HttpStatusCode StatusCode { get; }
+
+        public TValue Value { get; }
+
+        public HttpRequest RequestMessage { get; }
+
+        public Task ExecuteResultAsync(ActionContext context)
+        {
+            var result = new ObjectResult(Value)
+            {
+                DeclaredType = typeof(TValue),
+                StatusCode = (int)StatusCode
+            };
+            return result.ExecuteResultAsync(context);
+        }
+    }
+#else
+    public class AcrObjectResult<TValue> : IHttpStatusCodeActionResult
+    {
+        public AcrObjectResult(HttpStatusCode statusCode, TValue value, HttpRequestMessage requestMessage)
+        {
+            StatusCode = statusCode;
+            Value = value;
+            RequestMessage = requestMessage;
+        }
+
+        public HttpStatusCode StatusCode { get; }
+
+        public TValue Value { get; }
+
+        public HttpRequestMessage RequestMessage { get; }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = RequestMessage.CreateResponse<TValue>(StatusCode, Value);
+            return Task.FromResult(response);
+        }
+    }
+#endif
+}
--- WebAPI.Service1/Controllers/ValuesController.cs
+++ WebAPI.Service1/Controllers/ValuesController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Net;
 #if NETFRAMEWORK
 using System.Web.Http;
 #else
@@ -28,63 +23,4 @@ namespace WebAPI.Service1
             return new AcrObjectResult<Car>(HttpStatusCode.OK, new Car { Make = "Tesla" }, Request);
         }
     }
-
-    public interface IHttpStatusCodeActionResult : IHttpActionResult
-    {
-        HttpStatusCode StatusCode { get; }
-    }
-
-#if !NETFRAMEWORK
-    public abstract class ApiController : ControllerBase { }
-
-    public interface IHttpActionResult { }
-
-    public class AcrObjectResult<TValue> : IHttpStatusCodeActionResult, IActionResult
-    {
-        public AcrObjectResult(HttpStatusCode statusCode, TValue value, HttpRequest request)
-        {
-            StatusCode = statusCode;
-            Value = value;
-            RequestMessage = request;
-        }
-
-        public HttpStatusCode StatusCode { get; }
-
-        public TValue Value { get; }
-
-        public HttpRequest RequestMessage { get; }
-
-        public Task ExecuteResultAsync(ActionContext context)
-        {
-            var result = new ObjectResult(Value)
-            {
-                DeclaredType = typeof(TValue),
-                StatusCode = (int)StatusCode
-            };
-            return result.ExecuteResultAsync(context);
-        }
-    }
-#else
-    public class AcrObjectResult<TValue> : IHttpStatusCodeActionResult
-    {
-        public AcrObjectResult(HttpStatusCode statusCode, TValue value, HttpRequestMessage requestMessage)
-        {
-            StatusCode = statusCode;
-            Value = value;
-            RequestMessage = requestMessage;
-        }
-
-        public HttpStatusCode StatusCode { get; }
-
-        public TValue Value { get; }
-
-        public HttpRequestMessage RequestMessage { get; }
-
-        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
-        {
-            var response = RequestMessage.CreateResponse<TValue>(StatusCode, Value);
-            return Task.FromResult(response);
-        }
-    }
-#endif
 }
